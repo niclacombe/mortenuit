beb8570e3c1381dfa408cc94dfaeb4cc9c95aee1
niclacombe <niclacombe@gmail.com>
2017-02-02 13:01:30 -0500

    Debut formulaire inscription

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,14 +1,7 @@
-    <!-- jQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    
+	</div> <!-- #wrapper -->
 
-    <!-- Bootstrap Core JavaScript -->
-    <script src="/mortenuit/vendor/bootstrap/js/bootstrap.min.js"></script>
-
-    <!-- Metis Menu Plugin JavaScript -->
-    <script src="/mortenuit/vendor/metisMenu/metisMenu.min.js"></script>
-
-    <!-- Custom Theme JavaScript -->
-    <script src="/mortenuit/dist/js/sb-admin-2.js"></script>
+    
 
 </body>
 
---
+++ application/views/user/register.php
@@ -0,0 +1,74 @@
+<div class="page-wrapper">
+	<div class="row">
+		<div class="col-xs-12">
+			<h2>Inscription</h2>
+
+			<?php 
+				$data = array(
+					'class'	=> 'col-md-8 col-xs-12',
+				);
+				echo form_open('user/addUser',$data );
+
+					echo form_label('Prénom : ');
+					$data = array(
+						'type'	=> 'text',
+						'name'	=> 'prenom',
+						'class'	=> 'form-control',
+						'placeholder'	=> 'Prénom',
+					);
+					echo form_input($data);
+
+					echo form_label('Nom : ');
+					$data = array(
+						'type'	=> 'text',
+						'name'	=> 'nom',
+						'class'	=> 'form-control',
+						'placeholder'	=> 'Nom',
+					);
+					echo form_input($data);
+
+					echo form_label('Courriel : ');
+					$data = array(
+						'type'	=> 'email',
+						'name'	=> 'courriel',
+						'class'	=> 'form-control',
+						'placeholder'	=> 'Courriel',
+					);
+					echo form_input($data);
+
+					echo form_label('Mot de Passe : ');
+					$data = array(
+						'type'	=> 'text',
+						'name'	=> 'password',
+						'class'	=> 'form-control',
+						'placeholder'	=> 'Mot de Passe',
+					);
+					echo form_input($data);
+
+					echo form_label('Confirmation du Mot de Passe : ');
+					$data = array(
+						'type'	=> 'text',
+						'name'	=> 'password2',
+						'class'	=> 'form-control',
+						'placeholder'	=> 'Mot de Passe',
+					);
+					echo form_input($data);
+
+					$data = array(
+						'name'	=> 'confirmationAge',
+						'class'	=> 'form-check-input',
+						'id'	=> 'confirmationAge',
+					);
+					echo form_checkbox($data);
+					echo form_label("En m'inscrivant, je comprends que cette activité est réservée au 18 ans et plus.");
+
+					$data = array(
+						'class'	=> 'btn btn-primary',
+					);
+					echo form_submit($data,'Soumettre');
+
+				echo form_close();
+			?>
+		</div>
+	</div>
+</div>
